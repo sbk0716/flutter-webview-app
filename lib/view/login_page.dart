fc1a905fba635a49ba33c65702e5f41802378c63
sbk0716 <uhey0716@gmail.com>
2023-03-05 03:24:22 +0900

    add login page

---
+++ lib/view/login_page.dart
@@ -0,0 +1,66 @@
+import 'package:adaptive_dialog/adaptive_dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:webview_app/provider.dart';
+import 'package:webview_app/view/web_page.dart';
+
+class InputPage extends ConsumerWidget {
+  const InputPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Create a controller for an editable text field.
+    final _controller = TextEditingController();
+    // Set the initial value to the text property of the TextEditingController instance.
+    _controller.text = 'https://finatext.com/';
+
+    final _provider = ref.watch(provider.notifier);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Input Page'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Clear the text property of the TextEditingController instance.
+              _controller.text = "";
+            },
+            icon: const Icon(Icons.autorenew),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                hintText: 'https://flutter.dev/',
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                // Set a value to the text property of the TextEditingController instance.
+                _provider.state = value;
+              },
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                if (_provider.state == "") {
+                  showOkAlertDialog(
+                    context: context,
+                    title: 'Enter URL!!',
+                  );
+                } else {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const WebPage()));
+                }
+              },
+              child: const Text('OPEN WEB'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
